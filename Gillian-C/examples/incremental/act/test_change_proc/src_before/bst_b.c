5fe0be15775acf168391801302654ea9f0f3483c
pmaksimo <p.maksimovic@imperial.ac.uk>
2021-04-28 19:45:04 +0200

    removing non-Amazon examples

--- Gillian-C/examples/incremental/act/test_change_proc/src_before/bst_b.c
+++
@@ -1,9 +0,0 @@
-#include "bst.h"
-
-BST *a_make_node(int v) {
-    BST *new_node = malloc(sizeof(BST));
-    new_node->value = v;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    return new_node;
-}
--- Gillian-C/examples/multifile/concrete/foo.c
+++
@@ -1,7 +0,0 @@
-#include "foo.h"
-
-int x = 3;
-
-static int bar() { return 3; }
-
-int foo() { return 4; }
